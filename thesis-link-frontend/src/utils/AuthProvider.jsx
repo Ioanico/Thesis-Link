95e775a06a323b78bff669130465949739d6f2f0
ioanico <brandtioan@yahoo.com>
2024-06-26 13:20:48 +0300

    Minor changes

--- thesis-link-frontend/src/utils/AuthProvider.jsx
+++ thesis-link-frontend/src/utils/AuthProvider.jsx
@@ -6,59 +6,57 @@ import { jwtDecode } from "jwt-decode";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    // State to hold the authentication token
-    const [token, setToken_] = useState(localStorage.getItem("token"));
-    const [user, setUser] = useState({
+  // State to hold the authentication token
+  const [token, setToken_] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState({
+    _id: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    role: "",
+  });
+
+  // Function to set the authentication token
+  const setToken = (newToken) => {
+    setToken_(newToken);
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      localStorage.setItem("token", token);
+      setUser(jwtDecode(token));
+    } else {
+      delete axios.defaults.headers.common["Authorization"];
+      localStorage.removeItem("token");
+      setUser({
         _id: "",
         first_name: "",
         last_name: "",
         email: "",
         role: "",
-    });
+      });
+    }
+  }, [token]);
 
-    // Function to set the authentication token
-    const setToken = (newToken) => {
-        setToken_(newToken);
-    };
+  // Memoized value of the authentication context
+  const contextValue = useMemo(
+    () => ({
+      user,
+      token,
+      setToken,
+    }),
+    [token, user]
+  );
 
-    useEffect(() => {
-        if (token) {
-            axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-            localStorage.setItem("token", token);
-            setUser(jwtDecode(token));
-        } else {
-            delete axios.defaults.headers.common["Authorization"];
-            localStorage.removeItem("token");
-            setUser({
-                _id: "",
-                first_name: "",
-                last_name: "",
-                email: "",
-                role: "",
-            });
-        }
-    }, [token]);
-
-    // Memoized value of the authentication context
-    const contextValue = useMemo(
-        () => ({
-            user,
-            token,
-            setToken,
-        }),
-        [token, user]
-    );
-
-    // Provide the authentication context to the children components
-    return (
-        <AuthContext.Provider value={contextValue}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // Provide the authentication context to the children components
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;
